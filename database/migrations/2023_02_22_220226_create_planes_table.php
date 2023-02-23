5ea771faf37be5d47fd74a8fa5c86a1b500e74b9
Luiz Henrique Santos <luiz.santos85@gmail.com>
2023-02-22 22:14:12 -0300

    iniciando projeto

---
+++ database/migrations/2023_02_22_220226_create_planes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlanesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('planes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('qtd_passengers');
+            $table->enum('class', ['economic','luxury']);
+            $table->bigInteger('brand_id')->unsigned();
+            $table->foreign('brand_id')->references('id')->on('brands')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('planes');
+    }
+}
